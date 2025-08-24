6ae74dc395da6945cddfebd72ba1c235995a1451
anup30 <anupbarua30@gmail.com>
2025-08-25 05:57:12 +0600

    v1.5

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
-    path('profile/', views.ProfileUpdateView.as_view(), name='profile'),
     path('verify/<uuid:token>/', views.verify_email, name='verify-email'),
 	path('profile/', views.ProfileView.as_view(), name='profile'),
     path('profile/edit/', views.ProfileUpdateView.as_view(), name='edit-profile'),
