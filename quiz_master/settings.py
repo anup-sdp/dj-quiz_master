5ab175d4263cb052adaaafdbfbaa2b9ff5c31137
anup30 <anupbarua30@gmail.com>
2025-08-26 04:04:33 +0600

    v2.2

--- quiz_master/settings.py
+++ quiz_master/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*2z&ep_nwx9nbj)@-8sqt1fofdm84eqyb(ph*@#clq%$al0xn$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
